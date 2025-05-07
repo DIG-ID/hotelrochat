c1b2c03a9c0a7b0b1af7e80a5abe7f3b8daa4b73
Daniel-Digid <daniel@dig.id>
2025-05-07 12:42:08 +0100

    d

--- template-parts/archives/unsere-zimmer/rooms-list.php
+++ template-parts/archives/unsere-zimmer/rooms-list.php
@@ -6,50 +6,57 @@
             </div>
         </div>
         <?php
-            $zimmer_query = new WP_Query([
-                'post_type' => 'unsere-zimmer',
-                'post_status' => 'publish',
-                'orderby'     => 'date',
-				'order'       => 'ASC',
-                'posts_per_page' => -1
-            ]);
+        $zimmer_query = new WP_Query([
+            'post_type' => 'unsere-zimmer',
+            'post_status' => 'publish',
+            'orderby'     => 'date',
+            'order'       => 'ASC',
+            'posts_per_page' => -1
+        ]);
 
-            if ($zimmer_query->have_posts()) : ?>
-            <div class="theme-grid">
-                <div class="col-span-2 md:col-span-6 xl:col-span-12 relative text-center group overflow-hidden mb-7">
-                    <?php while ($zimmer_query->have_posts()) : $zimmer_query->the_post();
-                        $post_link = get_permalink();
-                        $post_title = get_the_title(); 
-                    ?>
-                        <a href="<?php echo esc_url($post_link); ?>" class="text_link"><?php echo esc_html($post_title); ?></a><span class="text-gold">&nbsp;/&nbsp;</span>
-                    <?php
-                        endwhile;
-                        wp_reset_postdata(); 
-                    ?>
-                </div>
-            </div>
-            <div class="theme-grid">
-            <?php
-                while ($zimmer_query->have_posts()) : $zimmer_query->the_post();
-                $featured_img = get_the_post_thumbnail_url(get_the_ID(), 'zimmer-image');
-                $post_link = get_permalink();
-                $post_title = get_the_title(); 
+        if ($zimmer_query->have_posts()) : ?>
+        <div class="theme-grid">
+            <div class="col-span-2 md:col-span-6 xl:col-span-12 relative text-center group overflow-hidden mb-7">
+                <?php while ($zimmer_query->have_posts()) : $zimmer_query->the_post();
+                    $post_link = get_permalink();
+                    $post_title = get_the_title(); 
                 ?>
-                    <div class="col-span-2 md:col-span-3 xl:col-span-4 relative group overflow-hidden">
-                        <a href="<?php echo esc_url($post_link); ?>" class="block w-full h-full">
-                            <div class="w-full h-full bg-cover bg-center" style="background-image: url('<?php echo esc_url($featured_img); ?>');">
-                                <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                                    <h3 class="text-white text-lg font-semibold text-center px-4"><?php echo esc_html($post_title); ?></h3>
-                                </div>
-                                <img src="<?php echo esc_url($featured_img); ?>" alt="<?php echo esc_attr($post_title); ?>" class="invisible w-full h-full object-cover" />
-                            </div>
-                        </a>
-                    </div>
+                    <a href="<?php echo esc_url($post_link); ?>" class="text_link"><?php echo esc_html($post_title); ?></a><span class="text-gold">&nbsp;/&nbsp;</span>
                 <?php
                     endwhile;
-                    wp_reset_postdata();
-                endif;
+                    wp_reset_postdata(); 
                 ?>
             </div>
+        </div>
+        <div class="theme-grid">
+        <?php
+            while ($zimmer_query->have_posts()) : $zimmer_query->the_post();
+            $featured_img = get_the_post_thumbnail_url(get_the_ID(), 'zimmer-image');
+            $post_link = get_permalink();
+            $post_title = get_the_title(); 
+            ?>
+                <div class="col-span-2 md:col-span-3 xl:col-span-4 relative group overflow-hidden">
+                    <a href="<?php echo esc_url($post_link); ?>" class="block w-full h-full">
+                        <div class="w-full h-full bg-cover bg-center" style="background-image: url('<?php echo esc_url($featured_img); ?>');">
+                            <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
+                                <div class="text-center text-white px-4">
+                                    <div class="text-xl text-white mb-1"> - - </div>
+                                    <h3 class="rooms-overview-title text-white mb-1"><?php echo esc_html($post_title); ?></h3>
+                                    <div class="text-body text-white">
+                                        <?php echo esc_html(get_field('title')); ?>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <img src="<?php echo esc_url($featured_img); ?>" alt="<?php echo esc_attr($post_title); ?>" class="invisible w-full h-full object-cover" />
+                        </div>
+                    </a>
+                </div>
+            <?php
+                endwhile;
+                wp_reset_postdata();
+            endif;
+            ?>
+        </div>
     </div>
 </section>
