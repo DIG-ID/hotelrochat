e8404d71f3d279d625ea436c72848add56ccdb2e
Daniel-Digid <daniel@dig.id>
2025-05-13 11:49:50 +0100

    d

---
+++ template-parts/pages/page-content.php
@@ -0,0 +1,12 @@
+<section class="section-page-content py-10 overflow-hidden">
+	<div class="custom-container">
+        <div class="theme-grid">
+            <div class="col-span-2 md:col-span-6 xl:col-span-12">
+                <h1 class="main-title-bigger text-gold pb-0 pt-5"><?php the_title(); ?></h1>
+            </div>
+			<div class="col-span-2 md:col-span-6 xl:col-span-12">
+				<div class="section-page-content-wrapper"><?php the_content(); ?></div>
+			</div>
+		</div>
+	</div>
+</section>
